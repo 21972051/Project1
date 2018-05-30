597f9a1c8d51c3fa7a4fe3087d84a9445a947bd9
Paulus Angkodjojo <21972051@student.uwa.edu.au>
2018-05-30 09:48:13 +0800

    first commit

---
+++ public/project.js
@@ -0,0 +1,51 @@
+/*project.js
+Name: Paulus Angkodjojo
+Student Number: 21972051*/
+
+/*var connect=require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(5000,function(){
+	console.log('Server running on 5000');
+});*/
+
+//Password validation for log in
+function UserValidate() {
+    var name = /^[a-zA-Z]+$/
+    document.getElementById('PasswordError').innerHTML = '';
+    if (document.getElementById("Password").value.length < 6 || document.getElementById("Password").value.length > 12) {
+        document.getElementById('PasswordError').innerHTML = 'Incorrect Password';
+        return false;
+    }
+}
+
+//Validations for sign up
+function Validate() {
+    var name = /^[a-zA-Z]+$/
+    var email = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,})+$/
+    document.getElementById('FirstNameError').innerHTML = '';
+    document.getElementById('LastNameError').innerHTML = '';
+    document.getElementById('EmailError').innerHTML = '';
+    document.getElementById('PasswordError').innerHTML = '';
+
+    if(!name.test(document.getElementById("FirstName").value)){
+        document.getElementById('FirstNameError').innerHTML = 'Please enter valid name';
+        return false;
+    }
+    if (!name.test(document.getElementById("LastName").value)) {
+        document.getElementById('LastNameError').innerHTML = 'Please enter valid name';
+        return false;
+    }
+    if (!email.test(document.getElementById("Email").value)) {
+        document.getElementById('EmailError').innerHTML = 'Please enter valid email';
+        return false;
+    }
+    if (document.getElementById("Password").value.length < 6 || document.getElementById("Password").value.length > 12){
+        document.getElementById('PasswordError').innerHTML = 'Password is too long/short';
+        return false;
+    }
+}
+
+function modDate(){
+    var x= new Date(document.lastModified);
+	document.getElementById("Date").innerHTML = x;
+}
