635dbc538b781a90fa89784cc81fbd15e6e9d221
Paulus Angkodjojo <21972051@student.uwa.edu.au>
2018-05-31 15:48:53 +0800

    'Commit'

--- app.js
+++ app.js
@@ -22,6 +22,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+//opening jade files
+app.get('/about',function(req,res)){
+	res.render('aboutme.jade',{title:'aboutme'});
+})
+
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
